0490ad93193b78fd8490023db72cb57c6d8d5288
Sujan Kumar Mitra <mitrakumarsujan@gmail.com>
2020-06-24 11:10:16 +0530

    added url encode request

---
+++ src/main/java/com/skmproject/urlshortener/dto/UrlEncodeRequest.java
@@ -0,0 +1,15 @@
+package com.skmproject.urlshortener.dto;
+
+public class UrlEncodeRequest {
+
+	private String url;
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+}

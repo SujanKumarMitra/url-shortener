53f64dc1a702ac21e858d8b130038ee88a3dde1b
Sujan Kumar Mitra <mitrakumarsujan@gmail.com>
2020-06-24 10:28:22 +0530

    added docs

--- src/main/java/com/skmproject/urlshortener/util/UrlValidator.java
+++ src/main/java/com/skmproject/urlshortener/util/UrlValidator.java
@@ -1,5 +1,16 @@
 package com.skmproject.urlshortener.util;
 
+/**
+ * Used to check whether an given url is a valid or not
+ * 
+ * @author Sujan Kumar Mitra
+ *
+ */
 public interface UrlValidator {
+	/**
+	 * Returns url is valid not
+	 * @param url url to validate
+	 * @return {@code true} is valid, else {@code false}
+ 	 */
 	boolean isUrlValid(String url);
 }

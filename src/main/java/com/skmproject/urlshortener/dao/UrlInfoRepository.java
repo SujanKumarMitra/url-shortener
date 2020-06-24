9bb48ddb60d7628ef3076433b68741f90d16cbe0
Sujan Kumar Mitra <mitrakumarsujan@gmail.com>
2020-06-24 10:45:00 +0530

    Added docs

--- src/main/java/com/skmproject/urlshortener/dao/UrlInfoRepository.java
+++ src/main/java/com/skmproject/urlshortener/dao/UrlInfoRepository.java
@@ -5,6 +5,11 @@ import org.springframework.stereotype.Repository;
 
 import com.skmproject.urlshortener.model.DefaultUrlInfo;
 
+/**
+ * Performs CRUD operations on {@link DefaultUrlInfo}
+ * @author Sujan Kumar Mitra
+ *
+ */
 @Repository
 public interface UrlInfoRepository extends CrudRepository<DefaultUrlInfo, String> {	
 }
--- src/main/java/com/skmproject/urlshortener/dao/package-info.java
+++ src/main/java/com/skmproject/urlshortener/dao/package-info.java
@@ -1,1 +1,4 @@
+/**
+ * Contains data access object classes
+ */
 package com.skmproject.urlshortener.dao;
